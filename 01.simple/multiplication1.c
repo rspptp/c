d6b72b47ae4b8a3e852760a9c4e44c21d0d27345
sai pavan <sai pavan>
2014-08-24 15:10:21 +0530

    multiplication of two aquations

---
+++ 01.simple/multiplication1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+	{
+		int x,y;
+		printf("Enter the values of xandy for multaplication");
+		scanf("%d %d",&x,&y);
+		(x+y)(x-y);
+		printf("multaplication of (x+y)(x-y)is");
+	}
