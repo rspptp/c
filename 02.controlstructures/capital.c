ed769aa3d8ce7c88abfe087d4518c93995e7f23d
sai pavan <sai pavan>
2014-08-24 07:24:50 +0530

    gives capital

---
+++ 02.controlstructures/capital.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+	{
+		int m;
+		if(m==1)
+		{
+			printf("Delhi");
+			if(m==2)
+			printf("Chennai");
+			else
+			printf("Bangalore");
+		}
+	else;
+		printf("END");
+	}
+
