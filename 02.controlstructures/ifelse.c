1164754f37a7006855228cb430015c22935bfdd2
sai pavan <sai pavan>
2014-08-24 07:15:13 +0530

    Gives more number of outputs

---
+++ 02.controlstructures/ifelse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+	{
+		int m=;
+		printf("Enter the values of m\n");
+		if(m<3)
+		  printf("%d\n",m+1);
+		else
+		if(m<5)
+		  printf("%d\n",m+2);
+		else
+		if(m<7)
+		  printf("%d\n",m+3);
+		else
+		  printf("%d\n",m+4);
+	}
