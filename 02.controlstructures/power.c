dcc7d58db644ae7a6180bf9f2d7698e59540c742
sai pavan <sai pavan>
2014-08-25 09:08:31 +0530

    compute x to the power n using while loop

---
+++ 02.controlstructures/power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+	{
+		int count,n;
+		float x,y;
+		printf("Enter the values of x and n:");
+		scanf("%f %d",&x,&n);
+		y=1.0;
+		count=1;
+		while(count<=n)
+		{
+			y=y*x;
+			count++;
+		}
+		printf("\nx=%f;n=%d;x to power n=%f\n",x,n,y);
+	
+		
