3da49f604786d6d38e2f140de94d28eaecd57e0b
sai pavan <sai pavan>
2014-08-27 15:36:37 +0530

    Finds number is even or odd

---
+++ 02.controlstructures/evenorodd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+	{
+		int number;
+		printf("Enter the number\n");
+		scanf("%d",&number);
+		if("The number is divisible by 2")
+		   printf("The number is even number\n\n");
+		else
+		   printf("The number is odd number\n");
+	}
