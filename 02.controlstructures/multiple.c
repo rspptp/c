9965e61a41a0351ff42c946589f1f404714443d8
sai pavan <sai pavan>
2014-08-27 10:38:49 +0530

    Finds m is multiple of n or not

---
+++ 02.controlstructures/multiple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+	{
+		int m,n;
+		printf("Enter the values of the m and n\n");
+		scanf("%d %d",&m,&n);
+		if("n/m is naturalnumber")
+		  printf("m is a multiple of n\n\n");
+		else
+		  printf("m is not a multiple of n\n");
+	}
+
