d8ed58533a66e27c9cdec33c05746ca90c01484c
sai pavan <sai pavan>
2014-08-27 09:59:16 +0530

    Finds area and perimeter

---
+++ 01.simple/rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+	{
+		int x,y;
+		float area,perimeter;
+		printf("Enter the values of x and y\n");
+		scanf("%f %f",&area,&perimeter);
+		area=x*y;
+		perimeter=2*(x+y);
+		printf("%d %d is:%f\n",area);
+		printf("%d %d is:%f\n",perimeter);
+	}
