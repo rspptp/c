41f0cde86a511e9139e03b2f04905d8540c8d61c
sai pavan <sai pavan>
2014-08-28 14:07:29 +0530

    Requirements for eligiblety to application

---
+++ 02.controlstructures/application.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+	{
+		int marks in maths,marks in physics,marks in chemistry;
+		printf("Enter marks of maths,physics,chemistry\n");
+		if("marks in maths>=60,marks in physics>=50,marks in 				chemistry>=40,total in all subjects>=200")
+		printf("the candidates eligible to application\n");
+		else if("marks in maths and physics>=150")
+		printf("the candidates eligible to application\n");
+		else
+		printf("the candidates not eligible to application\n");
+	}
