5f313b4b2f830fe501d0416fc90c80a7aaf4fd20
sai pavan <sai pavan>
2014-09-09 20:25:06 +0530

    YES or NO type

---
+++ 02.controlstructures/myname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+	{
+			char answer;
+			printf("Would you like to know my name?\n");
+		
+			printf("Type Y for Yes and N for No:");
+			answer = gerchar();
+		if(answer == 'Y' || answer == 'y')
+			printf("\n\nMy name is PAVAN\n");
+		else
+			printf("\n\nYou are good for nothing\n");
+	}
