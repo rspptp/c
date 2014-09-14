3a3840134feb07568f89b81f5df4546eb819ec0b
sai pavan <sai pavan>
2014-09-14 07:31:25 +0530

    Addition of two numbers

--- 01.simple/add.c
+++ 01.simple/add.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 main()
 {
-int a,b,c;
-printf("enter the values of a and b for addition:\n");
-scanf("%d%d",&a,&b);
-c=a+b;
-printf("Addition of the numbers is:%d",c);
+	int a,b,c;
+	printf("enter the values of a and b for addition:\n");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("Addition of the numbers is:%d",c);
 }
 
